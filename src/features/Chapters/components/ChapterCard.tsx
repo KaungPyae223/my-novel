e8a73138b933cf09a6c1005696db4f0c0893905c
Kaung223 <kaungpyaeaung8123@gmail.com>
2025-08-20 16:22:44 +0630

    feat: add summary in the chapter card

--- src/features/Chapters/components/ChapterCard.tsx
+++ src/features/Chapters/components/ChapterCard.tsx
@@ -1,4 +1,13 @@
-import { Heart, Pause, Play, Share, Share2, X, ZoomIn, ZoomOut } from "lucide-react";
+import {
+  Heart,
+  Pause,
+  Play,
+  Share,
+  Share2,
+  X,
+  ZoomIn,
+  ZoomOut,
+} from "lucide-react";
 import React, { useEffect } from "react";
 import { languagesList } from "@/lib/languageData";
 
@@ -28,30 +37,39 @@ const ChapterCard = ({
   chapterData: any;
 }) => {
   const [fontSize, setFontSize] = React.useState<number>(16);
-
-  console.log(chapterData);
+  const [readType, setReadType] = React.useState<string>("content");
 
   const [translatedLanguage, setTranslatedLanguage] =
     React.useState<string>("");
   const searchParams = useSearchParams();
 
-  useEffect(() => {
-    const language = searchParams.get("language");
-    if (language && language !== translatedLanguage) {
-      setTranslatedLanguage(language);
-    }
-  }, [searchParams]);
-
   const addParams = useAddParams();
 
   useEffect(() => {
+    const language = searchParams.get("language");
+    const readTypeParam = searchParams.get("read_type");
+    if (language && language !== translatedLanguage) {
+      setTranslatedLanguage(language);
+    }
+    if (readTypeParam && readTypeParam !== readType) {
+      setReadType(readTypeParam);
+    }
+  }, [searchParams]);
+
+  useEffect(() => {
+    const params = [];
     if (
       translatedLanguage &&
       searchParams.get("language") !== translatedLanguage
     ) {
-      addParams([{ key: "language", value: translatedLanguage }]);
+      params.push({ key: "language", value: translatedLanguage });
     }
-  }, [translatedLanguage, addParams]);
+    if (readType && searchParams.get("read_type") !== readType) {
+      params.push({ key: "read_type", value: readType });
+    }
+
+    if (params.length) addParams(params);
+  }, [translatedLanguage, readType]);
 
   const { data, isLoading, error } = useFetchData(
     useGenerateQuery(`/chapters/${chapterID}`)
@@ -119,8 +137,8 @@ const ChapterCard = ({
 
   return (
     <div className="p-6 bg-white border border-gray-200 rounded-md shadow-sm">
-      <div className="flex items-center justify-between">
-        <div>
+      <div className="flex flex-col gap-4 justify-between">
+        <div className="border-b border-gray-200 pb-4">
           <p className="font-medium text-xl">
             Chapter {chapterData?.chapter}: {chapterData?.title}
           </p>
@@ -129,81 +147,107 @@ const ChapterCard = ({
             {chapterData?.novel?.total_chapters}
           </p>
         </div>
-        <div className="flex items-center gap-4">
-          <div>
-            <p className="text-xs mb-0.5 text-gray-800">Font Size</p>
-            <div className="flex h-8 items-center gap-2 rounded-md border border-gray-300 px-2 py-1">
-              <ZoomOut
-                className="size-4 cursor-pointer"
-                onClick={handleZoomOut}
-              />
-              <div className="border-l border-gray-300 h-4"></div>
-              <p className="text-sm pointer-events-none">{fontSize}</p>
-              <div className="border-l border-gray-300 h-4"></div>
-              <ZoomIn
-                className="size-4 cursor-pointer"
-                onClick={handleZoomIn}
-              />
+        <div className="flex flex-row  justify-between gap-4">
+          <div className="flex items-center gap-4">
+            <div>
+              <p className="text-xs mb-0.5 text-gray-800">Read Type</p>
+              <Select value={readType} onValueChange={(value) => setReadType(value)}>
+                <SelectTrigger className="w-[180px] h-8 items-center gap-2 rounded-md border border-gray-300 px-2 py-1">
+                  <SelectValue placeholder="Select read type" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Choose how to read</SelectLabel>
+                    <SelectItem value="content">Read Full Content</SelectItem>
+                    <SelectItem value="summary">Read Summary</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
             </div>
-          </div>
-
-          <div>
-            <p className="text-xs mb-0.5 text-gray-800">Voice</p>
-            <div className="flex h-8 items-center gap-2 rounded-md border border-gray-300 px-2 py-1">
-              {isPlaying === "pause" ? (
-                <Pause onClick={pauseVoice} className="size-4 cursor-pointer" />
-              ) : isPlaying === "resume" ? (
-                <Play onClick={resumeVoice} className="size-4 cursor-pointer" />
-              ) : (
-                <Play onClick={playVoice} className="size-4 cursor-pointer" />
-              )}
-              <div className="border-l border-gray-300 h-4"></div>
-              <X onClick={stopVoice} className="size-4 cursor-pointer" />
-            </div>
-          </div>
-          <div>
-            <p className="text-xs mb-0.5 text-gray-800">Translation</p>
-            <Select
-              value={translatedLanguage}
-              onValueChange={(value) => setTranslatedLanguage(value)}
-            >
-              <SelectTrigger className="w-[180px] h-8 items-center gap-2 rounded-md border border-gray-300 px-2 py-1">
-                <SelectValue placeholder="Select a language" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Languages</SelectLabel>
-                  {languagesList.map((language) => (
-                    <SelectItem key={language} value={language}>
-                      {language}
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
-          <div>
-            <p className="text-xs mb-0.5 text-gray-800">{"General"}</p>
-            <div className="flex h-8 items-center gap-2 rounded-md border border-gray-300 px-2 py-1">
-              <motion.div
-                key={chapterData?.already_love ? "filled" : "unfilled"} // forces animation when state changes
-                initial={{ scale: 0.8, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                exit={{ scale: 0.8, opacity: 0 }}
-                transition={{ duration: 0.3 }}
-                onClick={handleLoved}
+            <div>
+              <p className="text-xs mb-0.5 text-gray-800">Translation</p>
+              <Select
+                value={translatedLanguage}
+                onValueChange={(value) => setTranslatedLanguage(value)}
               >
-                {chapterData?.already_love ? (
-                  <Heart
+                <SelectTrigger className="w-[180px] h-8 items-center gap-2 rounded-md border border-gray-300 px-2 py-1">
+                  <SelectValue placeholder="Select a language" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Languages</SelectLabel>
+                    {languagesList.map((language) => (
+                      <SelectItem key={language} value={language}>
+                        {language}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+          <div className="flex items-center gap-4">
+            <div>
+              <p className="text-xs mb-0.5 text-gray-800">Font Size</p>
+              <div className="flex h-8 items-center gap-2 rounded-md border border-gray-300 px-2 py-1">
+                <ZoomOut
+                  className="size-4 cursor-pointer"
+                  onClick={handleZoomOut}
+                />
+                <div className="border-l border-gray-300 h-4"></div>
+                <p className="text-sm pointer-events-none">{fontSize}</p>
+                <div className="border-l border-gray-300 h-4"></div>
+                <ZoomIn
+                  className="size-4 cursor-pointer"
+                  onClick={handleZoomIn}
+                />
+              </div>
+            </div>
+
+            <div>
+              <p className="text-xs mb-0.5 text-gray-800">Voice</p>
+              <div className="flex h-8 items-center gap-2 rounded-md border border-gray-300 px-2 py-1">
+                {isPlaying === "pause" ? (
+                  <Pause
+                    onClick={pauseVoice}
                     className="size-4 cursor-pointer"
-                    fill="currentColor"
+                  />
+                ) : isPlaying === "resume" ? (
+                  <Play
+                    onClick={resumeVoice}
+                    className="size-4 cursor-pointer"
                   />
                 ) : (
-                  <Heart className="size-4 cursor-pointer" />
+                  <Play onClick={playVoice} className="size-4 cursor-pointer" />
                 )}
-              </motion.div>
-              <div className="border-l border-gray-300 h-4"></div>
-              <Share onClick={shareLink} className="size-4 cursor-pointer" />
+                <div className="border-l border-gray-300 h-4"></div>
+                <X onClick={stopVoice} className="size-4 cursor-pointer" />
+              </div>
+            </div>
+
+            <div>
+              <p className="text-xs mb-0.5 text-gray-800">{"General"}</p>
+              <div className="flex h-8 items-center gap-2 rounded-md border border-gray-300 px-2 py-1">
+                <motion.div
+                  key={chapterData?.already_love ? "filled" : "unfilled"} // forces animation when state changes
+                  initial={{ scale: 0.8, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  exit={{ scale: 0.8, opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                  onClick={handleLoved}
+                >
+                  {chapterData?.already_love ? (
+                    <Heart
+                      className="size-4 cursor-pointer"
+                      fill="currentColor"
+                    />
+                  ) : (
+                    <Heart className="size-4 cursor-pointer" />
+                  )}
+                </motion.div>
+                <div className="border-l border-gray-300 h-4"></div>
+                <Share onClick={shareLink} className="size-4 cursor-pointer" />
+              </div>
             </div>
           </div>
         </div>
@@ -215,8 +259,10 @@ const ChapterCard = ({
       >
         {isLoading ? (
           <AiLoading text="Translating content..." />
-        ) : (
+        ) : readType === "content" ? (
           data.data.content
+        ) : (
+          data.data.summary
         )}
       </div>
     </div>
